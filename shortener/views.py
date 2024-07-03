7eefb565eb325515c61ececbf48bba59bf452b6f
mariepearlakoto <marietheedev@gmail.com>
2024-07-03 14:57:09 +0000

    designed webpage and linked to the backend
    created the index.html file and did the necessary html css and js codes there . Successfully linked frontend to backend.

--- shortener/views.py
+++ shortener/views.py
@@ -5,22 +5,29 @@ from django.http import HttpResponse
 
 
 
-# Create your views here.
+#First view that renders the index.html page
 
 def index(request):
+    
     return render(request, 'index.html')
 
+#View responsible for creating the shortened url
+
 def create(request):
-    if request.method == 'POST':
-        link = request.POST['link']
-        uid = str(uuid.uuid4())[:5]
-        
-        new_url = Url(link = link , uuid = uid)
-        new_url.save()
-        
-        return HttpResponse(uid)
     
+    if request.method == 'POST':        #checks if request is a post method so it handles it
+        
+        link = request.POST['link']     #stores the link the user types in the variable called link
+        uid = str(uuid.uuid4())[:5]     #generates a shorter uuid of 5 characters and stores it in the uid field
+        
+        new_url = Url(link = link , uuid = uid)    #stores the link and its respective uid in a variable called new_url
+        new_url.save()                             #saves the new url field into the database
+        
+        return HttpResponse(uid)                   #returns the shortened url to the user
     
+#View responsible for redorecting to the new url
+
 def go(request, pk):
-    url_details = Url.objects.get(uuid = pk)
-    return redirect(url_details.link)
+    
+    url_details = Url.objects.get(uuid = pk)       #Stores the link object whose uuid is equal to the search parameter
+    return redirect(url_details.link)              #Redirects to the original link using the link atrribute
